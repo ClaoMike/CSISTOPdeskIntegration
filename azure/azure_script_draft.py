01fff8bd485b7e86e9f9bd0b2e3d39f3dae1131a
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2025-12-15 14:07:52 +0100

    Removed all warnings

--- azure/azure_script_draft.py
+++ azure/azure_script_draft.py
@@ -723,14 +723,14 @@ config.csis_client_token = CsisAPI.get_token()
 csisAPI.set_authorization_token()
 ########################################################################################################################
 # Identify CSIS tickets that need to be created in TOPdesk
-tickets = csisAPI.get_tickets_to_be_created()
+tickets_to_be_created = csisAPI.get_tickets_to_be_created()
 TimestampUtils.save_current_date_as(config.last_new_tickets_timestamp_key)
 
 # Create the tickets in TOPdesk
-created_tickets = topdeskAPI.create_tickets(tickets)
+topdesk_created_tickets = topdeskAPI.create_tickets(tickets_to_be_created)
 
 # Update CSIS tickets with the corresponding TOPdesk ticket IDs
-csisAPI.update_tickets_with_customer_reference(created_tickets)
+csisAPI.update_tickets_with_customer_reference(topdesk_created_tickets)
 ########################################################################################################################
 # Fetch CSIS tickets that have been modified recently
 tickets_to_be_updated = csisAPI.get_tickets_to_be_updated()
